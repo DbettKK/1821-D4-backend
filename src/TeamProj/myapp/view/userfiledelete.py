27daea1f764daad02058f9f92d59a0f665e26b3b
haosheng055 <54499706+haosheng055@users.noreply.github.com>
2020-08-12 17:01:18 +0800

    userfiledelete的更新

--- src/TeamProj/myapp/view/userfiledelete.py
+++ src/TeamProj/myapp/view/userfiledelete.py
@@ -13,13 +13,12 @@ def chk_file_id(file_id):
         }, status=403)
     return f
 
-
+#回收站api，添加或恢复
 class FileIsDelete(APIView):
-    def post(self, request):
+    def get(self, request):
         token = request.META.get('HTTP_TOKEN')
         file_id = request.GET.get('file_id')
         is_delete = request.GET.get('is_delete')
-
         user_id = chk_token(token)
         if isinstance(user_id, Response):
             return user_id
@@ -45,9 +44,9 @@ class FileIsDelete(APIView):
             'code': 200,
         }, status=200)
 
-
+#在回收站中彻底删除
 class FileRealDelete(APIView):
-    def post(self, request):
+    def get(self, request):
         token = request.META.get('HTTP_TOKEN')
         file_id = request.GET.get('file_id')
 
