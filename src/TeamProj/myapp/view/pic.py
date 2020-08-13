ae4ead1f7f3f520c615fa4c9d4bbe6ab1a811f7b
haosheng055 <54499706+haosheng055@users.noreply.github.com>
2020-08-13 20:38:25 +0800

    修复提交重名图片的bug

--- src/TeamProj/myapp/view/pic.py
+++ src/TeamProj/myapp/view/pic.py
@@ -9,8 +9,7 @@ from django.core.mail import send_mail
 import datetime
 import os,json
 
-
-
+count=1
 
 # 发往：/picSave
 # 返回：/picSave/filename.
@@ -19,12 +18,15 @@ class getPic(APIView):
     def post(self,request):
         myFile = request.FILES.get("image", None)    # 获取上传的文件，如果没有文件，则默认为None  
         if not myFile:  
-            return Response("no files for upload!")  
-        destination = open(os.path.join(settings.MEDIA_ROOT,myFile.name),'wb+')    # 打开特定的文件进行二进制的写操作  
+            return Response("no files for upload!")
+        destination = open(os.path.join(settings.MEDIA_ROOT, myFile.name),'wb+')    # 打开特定的文件进行二进制的写操作  
         for chunk in myFile.chunks():      # 分块写入文件  
             destination.write(chunk)  
         destination.close()
+        old_dir=os.path.join(settings.MEDIA_ROOT, myFile.name)
+        new_dir=os.path.join(settings.MEDIA_ROOT, myFile.name + str(count).zfill(4)+ os.path.splitext(myFile)[1])
+        os.rename(old_dir,new_dir)
         return Response({
                 'info': '上传成功',
                 'code': 200,
-            }, status=200)    
+            }, status=200)
