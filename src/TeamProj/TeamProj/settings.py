3c06dc8080b64baaeb1d9e2663d7ac0277ea2155
ubuntu <ubuntu@localhost.localdomain>
2021-12-13 11:15:06 +0800

    deploy

--- src/TeamProj/TeamProj/settings.py
+++ src/TeamProj/TeamProj/settings.py
@@ -154,6 +154,6 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR,'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
